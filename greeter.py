75b09af512f1d23bcadf107763bd8087c1f297a5
Carsten Brachem <carsten.brachem@sportalliance.com>
2026-05-22 13:11:05 +0200

    support --shout flag

--- greeter.py
+++ greeter.py
@@ -1,15 +1,17 @@
 import argparse
 
 
-def greet(name: str) -> str:
-    return f"Hello, {name}!"
+def greet(name: str, shout: bool = False) -> str:
+    msg = f"Hello, {name}!"
+    return msg.upper() if shout else msg
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("--name", default="World")
+    parser.add_argument("--shout", action="store_true")
     args = parser.parse_args()
-    print(greet(args.name))
+    print(greet(args.name, args.shout))
 
 
 if __name__ == "__main__":
